2dfc7efd99b3c1279ebf63e3198ae4c1fd457f83
321MatiasRocky123 <matisg2129@gmail.com>
2025-08-21 18:24:27 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit",(e)=>{
+    e.preventDefault();
+var pesos = document.getElementById("pesos");
+ var dolar = 1298.50;
+var pesos = pesos.value;
+var total = Math.round(pesos / dolar)
+Swal.fire({
+  title: "Cantidad:" + (" $") + (total),
+  text: "¡Felicitaciones, puedes comprar dolares!",
+  icon: "success"
+});
+})
